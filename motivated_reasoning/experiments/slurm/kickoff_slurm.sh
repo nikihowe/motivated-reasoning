afa47c446747f0fd3e0ca775236f7ff576a61e15
Nikolaus (Niki) Howe <nikihowe@rnn.ist.berkeley.edu>
2025-08-06 17:30:00 -0700

    Revert "Decrease resources"
    This reverts commit c25aaec3bd7633378a95d88714b6ca1a7f20f087.

--- motivated_reasoning/experiments/slurm/kickoff_slurm.sh
+++ motivated_reasoning/experiments/slurm/kickoff_slurm.sh
@@ -18,9 +18,9 @@ else
 fi
 
 # SLURM job parameters
-SLURM_CPUS_PER_TASK=4
-SLURM_MEM="80gb" # May require up to 100gb per GPU for bigger models
-SLURM_GPUS="1" # IF USING MORE GPUS, NEED TO INCREASE MEMORY PROPORTIONALLY
+SLURM_CPUS_PER_TASK=8
+SLURM_MEM="100gb" # May require up to 100gb per GPU for bigger models
+SLURM_GPUS="4" # IF USING MORE GPUS, NEED TO INCREASE MEMORY PROPORTIONALLY
 GPU_TYPE="either" # A100 (faster generation) or A6000 (often more available), "either" (for either A100 or A6000), 
 #or "all" (for all available GPUs, will break most jobs). PCI for A100-PCI and SXM4 for A100-SXM4. SXM4 should be used for training LLama-3.1-70B
 SLURM_TIME="20:00:00"
