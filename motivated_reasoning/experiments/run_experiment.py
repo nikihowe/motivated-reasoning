28cb73dde8a087605f995a287c78c4f198abceae
Nikolaus (Niki) Howe <nikihowe@rnn.ist.berkeley.edu>
2025-08-18 12:24:55 -0700

    Run experiment

--- motivated_reasoning/experiments/run_experiment.py
+++ motivated_reasoning/experiments/run_experiment.py
@@ -12,7 +12,8 @@ from motivated_reasoning.utils.utils import find_freest_gpus
 # DEFAULT_CONFIG_PATH = "even_numbers.yaml"
 # DEFAULT_CONFIG_PATH = "first_second.yaml"
 # DEFAULT_CONFIG_PATH = "harmbench_random.yaml"
-DEFAULT_CONFIG_PATH = "harmbench_static_harmful_full_cot.yaml"
+# DEFAULT_CONFIG_PATH = "harmbench_static_harmful_full_cot.yaml"
+DEFAULT_CONFIG_PATH = "harmbench_cot_no_hints.yaml"
 
 
 def parse_args():
