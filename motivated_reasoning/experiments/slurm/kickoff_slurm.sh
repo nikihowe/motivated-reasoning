d4d791d39996dae7da595c6a7aafca0c37d4eb6b
Nikolaus (Niki) Howe <nikihowe@rnn.ist.berkeley.edu>
2025-08-18 16:00:29 -0700

    Decrease exp time

--- motivated_reasoning/experiments/slurm/kickoff_slurm.sh
+++ motivated_reasoning/experiments/slurm/kickoff_slurm.sh
@@ -23,8 +23,8 @@ SLURM_MEM="100gb" # May require up to 100gb per GPU for bigger models
 SLURM_GPUS="4" # IF USING MORE GPUS, NEED TO INCREASE MEMORY PROPORTIONALLY
 GPU_TYPE="either" # A100 (faster generation) or A6000 (often more available), "either" (for either A100 or A6000), 
 #or "all" (for all available GPUs, will break most jobs). PCI for A100-PCI and SXM4 for A100-SXM4. SXM4 should be used for training LLama-3.1-70B
-SLURM_TIME="20:00:00"
-SLURM_QOS="default" # can set to high if this is blocking your progress and you only need one/two jobs to run
+SLURM_TIME="6:00:00"
+SLURM_QOS="high" # can set to high if this is blocking your progress and you only need one/two jobs to run
 
 ###############################################################
 
